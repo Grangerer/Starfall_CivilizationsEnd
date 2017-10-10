f53bacbc6efa3de30b013f0210283f40b90bd731
Falco Schmitt <f.schmitt@profi-ag.de>
2017-10-10 15:56:52 +0200

    *Eventlog groundwork

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event {
+
+	/* ID	Group
+	 * 0	Building
+	 * 1	Spaceship
+	 * 2	Colonization
+	 * 3	Combat
+	 * 4	Research
+	 */	 
+	int groupID;
+	string description;
+	int turn;
+
+	public Event(string description, int turn, int groupID){
+		if (groupID < 0 || groupID > 4) {
+			Debug.Log ("Invalid Group ID!");
+		} else {
+			this.groupID = groupID;
+			this.description = description;
+			this.turn = turn;
+		}
+			
+	}
+
+
+	//Propertystuff
+	public int GroupID {
+		get {
+			return groupID;
+		}
+	}
+
+	public string Description {
+		get {
+			return description;
+		}
+	}
+
+	public int Turn {
+		get {
+			return turn;
+		}
+	}
+}
---
+++ Assets/Scripts/EventLog.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventLog : MonoBehaviour {
+
+	//Buildings ID=0
+	List<Event> buildEvents;
+	bool buildEventsDeactivated = false;
+
+	//Spaceships ID=1
+	List<Event> constructEvents; 
+	bool constructEventsDeactivated = false;
+
+	//Planet colonization ID=2
+	List<Event> collonadeEvents;
+	bool collonadeEventsDeactivated = false;
+
+	//Combat ID=3
+	List<Event> combatEvents;
+	bool combatEventsDeactivated = false;
+
+	//Research ID=4
+	List<Event> researchEvents;
+	bool researchEventsDeactivated = false;
+
+	public void AddEvent(Event newEvent){
+		switch (newEvent.GroupID) {
+		case 0:
+			buildEvents.Add (newEvent);
+			break;
+		case 1:
+			constructEvents.Add (newEvent);
+			break;
+		case 2:
+			collonadeEvents.Add (newEvent);
+			break;
+		case 3:
+			combatEvents.Add (newEvent);
+			break;
+		case 4:
+			researchEvents.Add (newEvent);
+			break;
+		default:
+			break;
+		}
+	}
+
+	/*
+	 * Display all currently activated events in the right order (Most recent turn first)
+	 */
+	public void DisplayLog(){
+		//For each activated:
+		//Find all events of turn 0 and add them to the Displaylog
+		//Increase turn by 1 and repeat until current turn is reached.
+	}
+}
